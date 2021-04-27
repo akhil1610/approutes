d2bc21becbcabc14ec6ca4f0f60d6c307268dce8
akhil1610 <akhilb.1610@gmail.com>
2021-04-27 12:10:44 +0530

    Create a React app and publish it to GitHub Pages

---
+++ src/Component/Navbar/headder/Header.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Grid from '@material-ui/core/Grid';
+
+function Header() {
+  return (
+    <div>
+      <Grid container spacing="2">
+          <Grid xs="2">
+              <h1>akhil</h1>
+          </Grid>
+          <Grid xs="2">
+              <h1>akhil</h1>
+          </Grid>
+          <Grid xs="2">
+              <h1>akhil</h1>
+          </Grid>
+          <Grid xs="2">
+              <h1>akhil</h1>
+          </Grid>
+          <Grid xs="2">
+              <h1>akhil</h1>
+          </Grid>
+          <Grid xs="2">
+              <h1>akhil</h1>
+          </Grid>
+      </Grid>
+    </div>
+  )
+}
+
+export default Header
